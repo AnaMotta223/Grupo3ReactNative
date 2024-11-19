41ffc88be6b985122f50f7faba5562fde6d74fb2
Ana Luiza <anamotta2753@gmail.com>
2024-11-19 16:07:41 -0300

    Add base

---
+++ pataseabracos/src/@types/navigation.d.ts
@@ -0,0 +1,17 @@
+export type PropsStack = {
+    StackLogin: undefined;
+    StackHome: undefined;
+  }
+  
+  export type PropsTabs = {
+    TabsHome: undefined,
+    TabsProfile: undefined,
+    TabsSettings: undefined,
+  }
+  
+  declare global {
+    namespace ReactNavigation {
+      interface RootParamList extends PropsStack {}
+      interface RootParamList extends PropsTabs {}
+    }
+  }
---
+++ pataseabracos/src/screens/CadastroPet/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+export const CadastroPet = () => {
+    return (
+      <View>
+        <Text>index</Text>
+      </View>
+    )
+}
