e103a10fd28f3123e0df9cc4d6dc3313cae9cfa2
Matheustorck <matheusalves.storck@gmail.com>
2024-11-20 18:10:08 -0300

    Adicionando Visual Página Cadastro PETS

--- pataseabracos/src/screens/CadastroPet/index.tsx
+++ pataseabracos/src/screens/CadastroPet/index.tsx
@@ -1,12 +1,155 @@
-import { Text, View } from 'react'
-import React from 'react'
+import {
+  Image,
+  ImageBackground,
+  Text,
+  TextInput,
+  TextInputComponent,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import Fundo from "../../assets/fundoClaro2.jpg";
+import Seta from "../../assets/seta.png";
+import { styles } from "./style";
+import SelectDropdown from "react-native-select-dropdown";
+import SetaBaixo from '../../assets/setaBaixo.png' 
+
+type Emoji = {
+  title: string;
+};
+
+const tipos = [
+  {
+    title: "CACHORRO",
+  },
+  {
+    title: "GATO",
+  },
+  {
+    title: "PASSARO",
+  },
+  {
+    title:'PEIXE',
+  },
+  {
+    title:'COELHO',
+  },
+  {
+    title:'HAMSTER',
+  },
+
+];
+
+const sexos = [
+    {
+      title:'MACHO',
+    },
+    {
+      title:'FEMEA'
+    }
+
+];
 
 export const CadastroPet = () => {
-    return (
-      <View>
-        <Text>
 
-        </Text>
+  const [tipoSelecionado, setTipoSelecionado] = useState<Emoji | null>(null)
+
+  return (
+    <ImageBackground source={Fundo} style={styles.background}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Image source={Seta} style={styles.headerimg} />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>Cadastro de Animal</Text>
+        </View>
+        <View style={styles.content}>
+        
+        <View style={styles.dropDownCard}>
+        <SelectDropdown 
+        
+    data={tipos}
+    onSelect={(selectedItem, index) => {
+      console.log(selectedItem, index);
+    }}
+    renderButton={(selectedItem, isOpened) => {
+      return (
+        <View style={styles.dropdownButtonStyle}>
+   
+          <Text style={styles.dropdownButtonTxtStyle}>
+            {(selectedItem && selectedItem.title) || 'Tipos:'} 
+          </Text>
+          <Image source={SetaBaixo}  />
+          
+        </View>
+      );
+    }}
+    renderItem={(item, index, isSelected) => {
+      return (
+        <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#708D73'})}}>
+         
+          <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+        </View>
+      );
+    }}
+    showsVerticalScrollIndicator={false}
+    dropdownStyle={styles.dropdownMenuStyle}
+  />
+  </View>
+
+          <TextInput style={styles.contentInput} placeholder="Nome:" />
+
+          <TextInput style={styles.contentInput} placeholder="Raça:" />
+
+          <View style={styles.dropDownCard}>
+        <SelectDropdown 
+        
+    data={sexos}
+    onSelect={(selectedItem, index) => {
+      console.log(selectedItem, index);
+    }}
+    renderButton={(selectedItem, isOpened) => {
+      return (
+        <View style={styles.dropdownButtonStyle}>
+   
+          <Text style={styles.dropdownButtonTxtStyle}>
+            {(selectedItem && selectedItem.title) || 'Sexo:'}
+          </Text>
+          <Image source={SetaBaixo}  />
+          
+        </View>
+      );
+    }}
+    renderItem={(item, index, isSelected) => {
+      return (
+        <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#708D73'})}}>
+         
+          <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+        </View>
+      );
+    }}
+    showsVerticalScrollIndicator={false}
+    dropdownStyle={styles.dropdownMenuStyle}
+  />
+  </View>
+
+
+         
+
+          <TextInput style={styles.contentInput} placeholder="Idade:" />
+
+          <TextInput style={styles.contentInput} placeholder="Peso:" />
+
+          <TextInput style={styles.contentInput} placeholder="Observações:" />
+
+          <TextInput style={styles.contentInput} placeholder="Localidade:" />
+        </View>
+        <TouchableOpacity style={styles.footerBottom}>
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>Cadastrar</Text>
+          </View>
+        </TouchableOpacity>
       </View>
-    )
-}
+    </ImageBackground>
+  );
+};
--- pataseabracos/src/screens/Perfil/index.tsx
+++ pataseabracos/src/screens/Perfil/index.tsx
@@ -1,10 +1,132 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Alert,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { styles } from "./style";
+import Fundo from "../../assets/fundoClaro2.jpg";
+import GatoFundo from "../../assets/gatoFundo.png";
+import Perfil from "../../assets/fotoPerfil.png";
+import Cima from "../../assets/setaCima.png"
+import Baixo from "../../assets/setaBaixo.png"
 
-export const Perfil = () => {
-    return (
-      <View>
-        <Text>index</Text>
-      </View>
-    )
-}
+export const PerfilUsuario = () => {
+  
+  const [isAdocoesOpen, setIsAdocoesOpen] = useState(false);
+  const [isAnimaisOpen, setIsAnimaisOpen] = useState(false);
+  const [isConfiguracoesOpen, setIsConfiguracoesOpen] = useState(false);
+
+  const toggleAdocoes = () => {
+    setIsAdocoesOpen(!isAdocoesOpen);
+  };
+
+  const toggleAnimais = () => {
+    setIsAnimaisOpen(!isAnimaisOpen);
+  };
+
+  const toggleConfiguracoes = () => {
+    setIsConfiguracoesOpen(!isConfiguracoesOpen);
+  };
+
+
+  return (
+    <ImageBackground source={Fundo} style={styles.background} blurRadius={6}>
+
+      
+      <View style={styles.container}>
+
+      <ScrollView style={styles.scroll}>
+
+        <View style={styles.header}>
+          <Image source={GatoFundo} style={styles.capa} />
+        </View>
+
+        <View style={styles.profileArea}>
+          <Image source={Perfil} style={styles.profilePicture} />
+          <Text style={styles.name}>Noé da Silva</Text>
+        </View>
+
+
+        <View style={styles.content}>
+
+          <View style={[styles.contentCard, {height: isAdocoesOpen ? 150: 50}]}>
+            <View style={styles.cardContent}>
+            <Text style={styles.cardTitle}>Suas Adoções</Text>
+            <TouchableOpacity style={styles.contentCardButton} onPress={toggleAdocoes}>
+              <Image source={isAdocoesOpen ? Cima : Baixo}/>
+            </TouchableOpacity>
+            </View>
+            {isAdocoesOpen && (
+              <View style = {styles.extraInfo}>
+                <Text>As parada do bauglho das adoção</Text>
+              </View>
+            )}
+          </View>
+
+          <View style={[styles.contentCard, {height: isAnimaisOpen ? 150: 50}]}>
+            <View style={styles.cardContent}>
+            <Text style={styles.cardTitle}>Seus animais para adoção</Text>
+            <TouchableOpacity style={styles.contentCardButton} onPress={toggleAnimais}>
+              <Image source={isAnimaisOpen ? Cima : Baixo}/>
+            </TouchableOpacity>
+            </View>
+            {isAnimaisOpen && (
+              <View style = {styles.extraInfo}>
+                <Text>As parada do bauglho das adoção</Text>
+              </View>
+            )}
+          </View>
+
+          <View style={[styles.contentCard, {height: isConfiguracoesOpen ? 150: 50}]}>
+            <View style={styles.cardContent}>
+            <Text style={styles.cardTitle}>Configurações</Text>
+            <TouchableOpacity style={styles.contentCardButton} onPress={toggleConfiguracoes}>
+              <Image source={isConfiguracoesOpen ? Cima : Baixo}/>
+            </TouchableOpacity>
+            </View>
+            {isConfiguracoesOpen && (
+              <View style = {styles.extraInfo}>
+                <TouchableOpacity
+                onPress={() => Alert.alert ("Função ainda não adicionada :(")}>
+                  <Text>Modo Escuro</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                onPress={() => Alert.alert ("Função ainda não adicionada :(")}>
+                  <Text>Editar foto de Perfil</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                onPress={() => Alert.alert ("Função ainda não adicionada :(")}>
+                  <Text>Editar nome de usuario</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                onPress={() => Alert.alert ("Função ainda não adicionada :(")}>
+                  <Text>Editar capa de Perfil</Text>
+                </TouchableOpacity>
+                <Text>TEste</Text>
+                <Text>TEste</Text>
+                <Text>TEste</Text>
+
+
+              </View>
+            )}
+          </View>
+
+        </View>
+
+        <TouchableOpacity style = {styles.footerBotton}>
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>Sair</Text>
+          </View>
+        </TouchableOpacity>
+        
+        </ScrollView>
+
+        </View>
+    </ImageBackground>
+  );
+};
