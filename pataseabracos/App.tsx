065874ccee50c79c076e8e2304f32414bf70c8ff
Gustavo Leal <gustavolealprado@gmail.com>
2024-11-19 21:14:43 -0300

    Criado Perfil, criado Tab-Routes

--- pataseabracos/App.tsx
+++ pataseabracos/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { PerfilUsuario } from './src/screens/Perfil';
+import { Routes } from './src/routes';
 //import { Routers } from './src/routes';
 
 export default function App() {
 
   return (
     <>
-      <StatusBar style="auto" />
-      {/* <Routers /> */}
+      {/* <PerfilUsuario/> */}
+      <Routes />
     </>
   );
 }
